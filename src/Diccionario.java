62c21747e92f6f7bf1c7d74609f5fe8e261d925f
Hauser <seba.cuccorese@gmail.com>
2019-09-27 13:02:25 -0300

    Completado metodos sobre si hay poker, trio, o color. Diccionario Modificado para en base al parametro de AS = 13;

--- src/Diccionario.java
+++ src/Diccionario.java
@@ -5,19 +5,19 @@ public class Diccionario {
     Map<Integer,String> map=new HashMap<Integer,String>();
 
     public Diccionario() {
-        map.put(1, "AS");
-        map.put(2, "2");
-        map.put(3, "3");
-        map.put(4, "4");
-        map.put(5,"5");
-        map.put(6,"6");
-        map.put(7, "7");
-        map.put(8, "8");
-        map.put(9,"9");
-        map.put(10, "10");
-        map.put(11, "J");
-        map.put(12, "Q");
-        map.put(13, "K");
+        map.put(1, "2");
+        map.put(2, "3");
+        map.put(3, "4");
+        map.put(4,"5");
+        map.put(5,"6");
+        map.put(6, "7");
+        map.put(7, "8");
+        map.put(8,"9");
+        map.put(9, "10");
+        map.put(10, "J");
+        map.put(11, "Q");
+        map.put(12, "K");
+        map.put(13, "AS");
     }
     public Integer buscarCartaDiccionario(Carta carta) {
         int i = 1;
--- src/PokerStatus.java
+++ src/PokerStatus.java
@@ -1,16 +1,57 @@
 import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class PokerStatus {
 
+    public String verificar(Carta carta1, Carta carta2, Carta carta3, Carta carta4, Carta carta5) {
+        if(comparePoker(carta1, carta2, carta3, carta4, carta5)) {return "Es Poker";}
+        else if(compareTrio((carta1, carta2, carta3, carta4, carta5)) {return "Es Trio";}
+        else if(compareColor((carta1, carta2, carta3, carta4, carta5)) {return "Es Color";}
+        else {return  "No hay jugada";}
+    }
+
+    private boolean compareTrio(Carta carta1, Carta carta2, Carta carta3, Carta carta4, Carta carta5) {
+        ArrayList<Carta> mano = new ArrayList<>();
+        mano.addAll(Arrays.asList(carta1, carta2, carta3, carta4, carta5));
+
+        ArrayList<Carta> listaFiltradaPrimeraCarta = mano;
+        ArrayList<Carta> listaFiltradaSegundaCarta = mano;
+        ArrayList<Carta> listaFiltradaTerceraCarta = mano;
+            filtrarManoPorNumero(listaFiltradaPrimeraCarta,0);
+            filtrarManoPorNumero(listaFiltradaSegundaCarta, 1);
+            filtrarManoPorNumero(listaFiltradaTerceraCarta, 2);
+
+        return listaFiltradaPrimeraCarta.size() == 3 || listaFiltradaSegundaCarta.size() == 3 || listaFiltradaTerceraCarta.size() == 3;
+    }
+
     private boolean comparePoker(Carta carta1, Carta carta2, Carta carta3, Carta carta4, Carta carta5) {
+        ArrayList<Carta> mano = new ArrayList<>();
+        mano.addAll(Arrays.asList(carta1, carta2, carta3, carta4, carta5));
 
-    }
-    private boolean compareTwoCardsNumber(Carta carta1, Carta carta2) {
-        return carta1.getNumero().equals(carta2.getNumero());
+       ArrayList<Carta> listaFiltradaPrimeraCarta = mano;
+       ArrayList<Carta> listaFiltradaSegundaCarta = mano;
+            filtrarManoPorNumero(listaFiltradaPrimeraCarta,0);
+            filtrarManoPorNumero(listaFiltradaSegundaCarta, 1);
+
+        return listaFiltradaPrimeraCarta.size() == 4 || listaFiltradaSegundaCarta.size() == 4;
     }
 
-    private boolean compararSiLosPalosSonIguales(@NotNull Carta carta1, Carta carta2) {
-        return carta1.getPalo().equals(carta2.getPalo());
+    @NotNull
+    private List filtrarManoPorNumero(@NotNull List<Carta> cartaList, int i) {
+        List<Carta> listaFiltrada = cartaList.stream().filter(carta -> carta.getNumero().equals(cartaList.get(i).getNumero())).collect(Collectors.toList());
+
+
+        return listaFiltrada;
+    }
+    private boolean compareColor(Carta carta1, Carta carta2, Carta carta3, Carta carta4, Carta carta5) {
+        ArrayList<Carta> mano = new ArrayList<>();
+        mano.addAll(Arrays.asList(carta1, carta2, carta3, carta4, carta5));
+        List<Carta> listaColor = mano.stream().filter(carta -> carta.getPalo().equals(mano.get(0).getPalo())).collect(Collectors.toList());
+
+        return listaColor.size() == 5;
     }
 }
