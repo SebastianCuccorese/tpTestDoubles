01c583f9a3a73116ac20ade9b0db1570deeb9d38
Hauser <seba.cuccorese@gmail.com>
2019-09-27 13:08:28 -0300

    Optimizado el codigo

--- src/PokerStatus.java
+++ src/PokerStatus.java
@@ -15,41 +15,25 @@ public class PokerStatus {
     }
 
     private boolean compareTrio(Carta carta1, Carta carta2, Carta carta3, Carta carta4, Carta carta5) {
-        ArrayList<Carta> mano = new ArrayList<>();
-        mano.addAll(Arrays.asList(carta1, carta2, carta3, carta4, carta5));
+        ArrayList<Carta> mano = new ArrayList<>(Arrays.asList(carta1, carta2, carta3, carta4, carta5));
 
-        ArrayList<Carta> listaFiltradaPrimeraCarta = mano;
-        ArrayList<Carta> listaFiltradaSegundaCarta = mano;
-        ArrayList<Carta> listaFiltradaTerceraCarta = mano;
-            filtrarManoPorNumero(listaFiltradaPrimeraCarta,0);
-            filtrarManoPorNumero(listaFiltradaSegundaCarta, 1);
-            filtrarManoPorNumero(listaFiltradaTerceraCarta, 2);
-
-        return listaFiltradaPrimeraCarta.size() == 3 || listaFiltradaSegundaCarta.size() == 3 || listaFiltradaTerceraCarta.size() == 3;
+        return (filtrarManoPorNumero(mano, 0).size() ==3) || (filtrarManoPorNumero(mano, 1).size() == 3) || (filtrarManoPorNumero(mano, 0).size() == 3);
     }
 
     private boolean comparePoker(Carta carta1, Carta carta2, Carta carta3, Carta carta4, Carta carta5) {
-        ArrayList<Carta> mano = new ArrayList<>();
-        mano.addAll(Arrays.asList(carta1, carta2, carta3, carta4, carta5));
+        ArrayList<Carta> mano = new ArrayList<>(Arrays.asList(carta1, carta2, carta3, carta4, carta5));
 
-       ArrayList<Carta> listaFiltradaPrimeraCarta = mano;
-       ArrayList<Carta> listaFiltradaSegundaCarta = mano;
-            filtrarManoPorNumero(listaFiltradaPrimeraCarta,0);
-            filtrarManoPorNumero(listaFiltradaSegundaCarta, 1);
-
-        return listaFiltradaPrimeraCarta.size() == 4 || listaFiltradaSegundaCarta.size() == 4;
+        return filtrarManoPorNumero(mano,0).size() == 4 || filtrarManoPorNumero(mano,1).size() == 4;
     }
 
     @NotNull
     private List filtrarManoPorNumero(@NotNull List<Carta> cartaList, int i) {
-        List<Carta> listaFiltrada = cartaList.stream().filter(carta -> carta.getNumero().equals(cartaList.get(i).getNumero())).collect(Collectors.toList());
 
 
-        return listaFiltrada;
+        return cartaList.stream().filter(carta -> carta.getNumero().equals(cartaList.get(i).getNumero())).collect(Collectors.toList());
     }
     private boolean compareColor(Carta carta1, Carta carta2, Carta carta3, Carta carta4, Carta carta5) {
-        ArrayList<Carta> mano = new ArrayList<>();
-        mano.addAll(Arrays.asList(carta1, carta2, carta3, carta4, carta5));
+        ArrayList<Carta> mano = new ArrayList<>(Arrays.asList(carta1, carta2, carta3, carta4, carta5));
         List<Carta> listaColor = mano.stream().filter(carta -> carta.getPalo().equals(mano.get(0).getPalo())).collect(Collectors.toList());
 
         return listaColor.size() == 5;
