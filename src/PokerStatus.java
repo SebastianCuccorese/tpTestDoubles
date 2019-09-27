0a712ea825ad3e7eb2020430b72cac29f02dc900
Hauser <seba.cuccorese@gmail.com>
2019-09-27 14:59:57 -0300

    Optimizado nuevamente el codigo

--- src/PokerStatus.java
+++ src/PokerStatus.java
@@ -20,19 +20,18 @@ public class PokerStatus {
         return (filtrarManoPorNumero(mano, 0).size() ==3) || (filtrarManoPorNumero(mano, 1).size() == 3) || (filtrarManoPorNumero(mano, 0).size() == 3);
     }
 
-    private boolean comparePoker(Carta carta1, Carta carta2, Carta carta3, Carta carta4, Carta carta5) {
+    boolean comparePoker(Carta carta1, Carta carta2, Carta carta3, Carta carta4, Carta carta5) {
         ArrayList<Carta> mano = new ArrayList<>(Arrays.asList(carta1, carta2, carta3, carta4, carta5));
 
         return filtrarManoPorNumero(mano,0).size() == 4 || filtrarManoPorNumero(mano,1).size() == 4;
     }
 
-    @NotNull
-    private List filtrarManoPorNumero(@NotNull List<Carta> cartaList, int i) {
+    @NotNull List filtrarManoPorNumero(@NotNull List<Carta> cartaList, int i) {
 
 
         return cartaList.stream().filter(carta -> carta.getNumero().equals(cartaList.get(i).getNumero())).collect(Collectors.toList());
     }
-    private boolean compareColor(Carta carta1, Carta carta2, Carta carta3, Carta carta4, Carta carta5) {
+    boolean compareColor(Carta carta1, Carta carta2, Carta carta3, Carta carta4, Carta carta5) {
         ArrayList<Carta> mano = new ArrayList<>(Arrays.asList(carta1, carta2, carta3, carta4, carta5));
         List<Carta> listaColor = mano.stream().filter(carta -> carta.getPalo().equals(mano.get(0).getPalo())).collect(Collectors.toList());
 
